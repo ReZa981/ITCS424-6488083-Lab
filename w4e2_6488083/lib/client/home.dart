e5a7c13e47213169854480b37b2350f7ab14a623
Kanyanut S <kanyanutsompong@gmail.com>
2024-02-04 14:47:42 +0700

    create home page

---
+++ w4e2_6488083/lib/client/home.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'theme.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 50,
+        backgroundColor: Theme.of(context).colorScheme.background,
+        title: Align(
+          alignment: Alignment.centerLeft,
+          child: Row(
+            children: [
+              Image.asset("../asset/logo.png"),
+              const Text ('guragurra'),
+            ],
+          )
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
