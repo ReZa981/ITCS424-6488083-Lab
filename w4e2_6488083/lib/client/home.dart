d365fe7af5ff282d2d65a0cc140cc5f886cdc874
Kanyanut S <kanyanutsompong@gmail.com>
2024-02-04 21:16:34 +0700

    update anime list

--- w4e2_6488083/lib/client/home.dart
+++ w4e2_6488083/lib/client/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:footer/footer_view.dart';
 import 'theme.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,18 +10,60 @@ class HomePage extends StatefulWidget {
 
 List<Map<String, String>> animeList = [
   {
-    "animeOriginalName": "Jujutsu Kaisen",
-    "animeEngName": "呪術廻戦",
-    "animePoster": "jujutsu.png",
+    "animeOriginalName": "呪術廻戦",
+    "animeEngName": "Jujutsu Kaisen",
+    "animePoster": "https://m1r.ai/9/r35vt.jpg",
     "availablePlatform": "netflix",
   },
   {
     "animeOriginalName": "Solo leveling",
     "animeEngName": "Solo leveling",
-    "animePoster": "jujutsu.png",
+    "animePoster": "https://m1r.ai/9/2r5yx.jpg",
     "availablePlatform": "netflix",
   },
-  // Add more anime maps to the list as needed
+  {
+    "animeOriginalName": "ワンピース",
+    "animeEngName": "One Piece",
+    "animePoster": "https://m1r.ai/9/mm3nu.jpg",
+    "availablePlatform": "netflix",
+  },
+  {
+    "animeOriginalName": "ハウルの動く城",
+    "animeEngName": "Howl‘s Moving Castle",
+    "animePoster": "https://m1r.ai/9/mwp95.jpg",
+    "availablePlatform": "netflix",
+  },
+  {
+    "animeOriginalName": "となりのトトロ",
+    "animeEngName": "My Neighbor Totoro",
+    "animePoster": "https://m1r.ai/9/h8jza.jpg",
+    "availablePlatform": "netflix",
+  },
+  {
+    "animeOriginalName": "君に届け",
+    "animeEngName": "Kimi ni Todoke",
+    "animePoster": "https://m1r.ai/9/qidxj.jpg",
+    "availablePlatform": "netflix",
+  },
+  {
+    "animeOriginalName": "転生したらスライムだった件",
+    "animeEngName": "That Time I Got Reincarnated as a Slime",
+    "animePoster": "https://m1r.ai/9/njb0k.jpg",
+    "availablePlatform": "netflix",
+  },
+  {
+    "animeOriginalName": "ようこそ実力至上主義の教室へ ",
+    "animeEngName": "Classroom of the Elite Season",
+    "animePoster": "https://m1r.ai/9/4w17f.jpg",
+    "availablePlatform": "netflix",
+  },
+  {
+    "animeOriginalName": "マッシュル-MASHLE- 神覚者候補選抜試験編",
+    "animeEngName": "MASHLE: MAGIC AND MUSCLES",
+    "animePoster":
+        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx166610-IjJ8YLOKsua4.jpg",
+    "availablePlatform": "netflix",
+  },
 ];
 
 class _HomePageState extends State<HomePage> {
@@ -109,15 +152,15 @@ class _HomePageState extends State<HomePage> {
                 controller: ScrollController(keepScrollOffset: false),
                 shrinkWrap: true,
                 children: List.generate(
-                    9,
-                    (index) => AnimeCard(
-                          animeOriginalName: 'Original Name ${index + 1}',
-                          animeEngName: 'English Name ${index + 1}',
-                          animePoster:
-                              'jujutsu.png', // Make sure this asset exists
-                          availablePlatform: 'Platform ${index + 1}',
-                        )),
-              )
+                  animeList.length,
+                  (index) => AnimeCard(
+                    animeOriginalName: animeList[index]["animeOriginalName"]!,
+                    animeEngName: animeList[index]["animeEngName"]!,
+                    animePoster: animeList[index]["animePoster"]!,
+                    availablePlatform: animeList[index]["availablePlatform"]!,
+                  ),
+                ),
+              ),
             ],
           )),
     );
@@ -152,29 +195,54 @@ class AnimeCard extends StatelessWidget {
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10),
               image: DecorationImage(
-                image: AssetImage('./asset/$animePoster'),
+                image: NetworkImage(animePoster),
                 fit: BoxFit.fitHeight,
               ),
+            ),
+            child: Stack(
+              children: [
+                Positioned(
+                  bottom: 5,
+                  right: 5,
+                  child: Container(
+                    width: 25,
+                    height: 25,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('./asset/$availablePlatform.png'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
           Padding(
             padding: const EdgeInsets.all(4.0),
-            child: Text(
-              animeOriginalName,
-              style: TextStyle(
+            child: RichText(
+              overflow: TextOverflow.ellipsis,
+              strutStyle: StrutStyle(fontSize: 12.0),
+               text: TextSpan(
+                      style: TextStyle(
                 fontSize: 10,
                 color: Color(ColorPalatte.color['shadow']!),
               ),
+                      text: animeOriginalName ),
+                ),
             ),
-          ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 4.0),
-            child: Text(
-              animeEngName,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 12,
-              ),
+            child: RichText(
+              overflow: TextOverflow.ellipsis,
+              strutStyle: StrutStyle(fontSize: 12.0),
+              text: TextSpan(
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12,
+                  ),
+                  text: animeEngName),
             ),
           ),
         ],
